cfaa8a86352b6c16e2e633c5c1ba78c7baa2daaf
vess william <vess1994@gmail.com>
2024-09-15 23:59:38 +0800

    ready to render detail

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,13 +1,33 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { useObject } from "@realm/react";
+import { Task } from "../models/Task";
+import { BSON } from "realm";
 
 const TaskDetail = () => {
-  const { id } = useLocalSearchParams();
-  console.warn(id);
+  const { id } = useLocalSearchParams(); // Fetch ID from URL params
+  const task = useObject<Task>(Task, new BSON.ObjectID(id as string)); // Fetch Task from Realm
+
   return (
     <View style={{ padding: 10 }}>
-      <Text style={{ color: "white", fontSize: 20 }}>Id: {id}</Text>
+      {!task ? (
+        <Text style={{ color: "white", fontSize: 20 }}>
+          Task not found or still loading...
+        </Text>
+      ) : (
+        <>
+          <Text style={{ color: "white", fontSize: 30 }}>
+            {task.description}
+          </Text>
+          <Text style={{ color: "gray", fontSize: 20 }}>
+            Completed: {task.isComplete ? "Yes" : "No"}
+          </Text>
+          <Text style={{ color: "gray", fontSize: 20 }}>
+            Created At: {task.createdAt?.toDateString()}
+          </Text>
+        </>
+      )}
     </View>
   );
 };
